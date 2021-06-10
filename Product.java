19a3a7fed67887b0ac495084e6de1eb73503946c
tsh <13535153002@163.com>
2021-06-10 21:29:56 +0800

    主分支提交2

--- Product.java
+++ Product.java
@@ -1,6 +1,6 @@
 public class Product {
     public static void main(String[] args) {
         System.out.println("I'm version 9.00");
-        System.out.println("hahahahahahahaha");
+        System.out.println("wuwuwuwuwuwwuwuw");
     }
 } 
